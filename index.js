a10323d793d02a6b4bcfa036fec21d1b3e3e9558
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 03:16:38 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 
 const num1 = 31;
 const num2 = 2;
-let multiple = num1 * num2;
-let ran = Math.floor(Math.random() * 10) + 1;
+const num3 = 29;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3%5
+let max = Math.max(0,4,5,20)
