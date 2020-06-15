4c3c7857be21319ff862fd92b5b3dfd81c9a9808
jamaljfrank <jamaljfrank@gmail.com>
2020-06-15 00:48:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const num1 = 1;
+const num2 = Math.round(61.5);
 
-// Write your code here
+const multiply = num1 * num2;
+let random = Math.random();
+const mod = 12 % 8;
+const max = Math.max(1, 20);
