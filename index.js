87b6d62a466d6700186ddb53a412c9852bd2cafe
Warren Harper II <warrenharper2@att.net>
2020-06-16 15:59:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+
+
+ let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 0;
+mod += 10;
+mod %= 6;
+
+let max = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+Math.max(max)
